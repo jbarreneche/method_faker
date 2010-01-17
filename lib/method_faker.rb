fffa7eaf2007309ec07fd8015071824b2edf307b
Juan Manuel Barreneche <snipper_me85@yahoo.com.ar>
2010-01-17 14:14:13 -0300

    Simplifying and reorganizing

--- lib/method_faker.rb
+++ lib/method_faker.rb
@@ -1,11 +1,4 @@
 module MethodFaker
-  def self.extended(klass)
-    class << klass
-      def faked_methods
-        @faked_methods ||= { }
-      end
-    end
-  end
   def fake(method_name, proc = nil, &block)
     fail 'You can\'t fake that' if (method_name =~ /__|instance_eval/)
     faked_methods[method_name] ||= inquire_method(method_name)
@@ -22,12 +15,6 @@ module MethodFaker
   def faked?(method_name)
     faked_methods.keys.include? method_name
   end
-  def inquire_method(method_name)
-    return [instance_method(method_name), :public] if public_instance_methods.include? method_name
-    return [instance_method(method_name), :protected] if protected_instance_methods.include? method_name
-    return [instance_method(method_name), :private] if private_instance_methods.include? method_name
-    fail '#{method_name} it\'s not a method in class: #{self}'
-  end
 
 private
   def block_fake(method_name, proc, block)
@@ -36,7 +23,6 @@ private
   ensure
     restore(method_name)
   end
-
   def standard_fake(method_name, block)
     visibility = faked_methods[method_name].last
     define_method_with_visibility method_name, block, visibility
@@ -45,6 +31,17 @@ private
   def define_method_with_visibility(method_name, method, visibility)
     define_method method_name, method
     send visibility, method_name
+  end
+
+  def faked_methods
+    @faked_methods ||= { }
+  end
+
+  def inquire_method(method_name)
+    return [instance_method(method_name), :public] if public_instance_methods.include? method_name
+    return [instance_method(method_name), :protected] if protected_instance_methods.include? method_name
+    return [instance_method(method_name), :private] if private_instance_methods.include? method_name
+    fail '#{method_name} it\'s not a method in class: #{self}'
   end
 end
 
